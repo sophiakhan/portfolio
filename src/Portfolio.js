779949188c2b68aa863bdce1cf262b388eb72046
Sophia Khan <sophiaxkhan@gmail.com>
2017-03-24 09:58:50 -0400

    Transfered portfolio to a react app

---
+++ src/Portfolio.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class Portfolio extends Component {
+  render() {
+    return <div class="container">
+        <div class="row">
+            <div class="col-sm-7">
+        
+            </div>
+            <div class="col-sm-5">
+            </div>
+        </div>
+    </div>
+
+  }
+}
+
+export default Portfolio;
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './Portfolio';
+
+ReactDOM.render(
+  <Portfolio />,
+  document.getElementById('root')
+);
