84c79cfa5a678e1ffe9e3eb0a5b0b612137eca5c
Sophia Khan <sophiaxkhan@gmail.com>
2017-03-24 13:20:02 -0400

    Update about page

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,60 @@
 import React from 'react'
 
-const About = () => <div>About</div>
+const About = () => <div>
+<div className="row">
+    <div className="col-sm-7">
+        <div className="text-center"><img src="https://unsplash.it/5472/3648?image=1074" alt="Sophia Khan" /></div>
+    </div>
+    <div className="col-sm-5">
+        <h1 id="aboutHead">Using code to build clean and creative websites.</h1> 
+        <p id="aboutInfo">
+            Pug umami semiotics hashtag +1 truffaut banjo dolore four loko etsy. Meh odio fashion axe seitan franzen laborum voluptate commodo twee normcore portland duis. +1 cardigan velit do. Single-origin coffee organic vegan, deserunt lomo nostrud iPhone selvage celiac yuccie aliqua four loko.  Minim VHS jianbing, organic dolore keytar letterpress voluptate cold-pressed ramps proident meditation etsy.
+        </p><br/>
+    </div>
+        <h3><strong>Experience</strong></h3> <br/>
+        <div className="row">
+            <div className="col-sm-3">
+                <ul className="list-unstyled">
+                    <li><strong>Languages</strong></li>
+                    <li>HTML</li>
+                    <li>CSS</li>
+                    <li>SCSS</li>
+                    <li>JavaScript</li>
+                    <li>SQL</li>
+                </ul>
+            </div>
+            <div className="col-sm-3">
+                <ul className="list-unstyled">
+                    <li><strong>Tools</strong></li>
+                    <li>Yarn/npm</li>
+                    <li>Webpack</li>
+                    <li>Node.js</li>
+                    <li>Postico/Postgres</li>
+                    <li>Create React App</li>
+                </ul>
+            </div>
+            <div className="col-sm-3">
+                <ul className="list-unstyled">
+                    <li><strong>Libraries</strong></li>
+                    <li>React.js</li>
+                    <li>Bootstrap</li>
+                    <li>Bulma</li>
+                    <li>jQuery</li>
+                </ul>
+            </div>
+            <div className="col-sm-3">
+                <ul className="list-unstyled">
+                    <li><strong>Etc.</strong></li>
+                    <li>Responsive Design</li>
+                    <li>UI Design</li>
+                    <li>Adobe Creative Suite</li>
+                    <li>Content Writing</li>
+                    <li>Social Media Strategy</li>
+                </ul>
+            </div>
+        </div>
+    
+</div>
+</div>
 
 export default About
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,5 +1,19 @@
 import React from 'react'
 
-const Contact = () => <div>Contact</div>
+const Contact = () => <div>
+<div  className="text-center">
+    <h1 id="letsChat">Let's chat</h1>
+     <h4><span className="glyphicon glyphicon-envelope contactInfo">  sophiaxkhan@gmail.com</span></h4> 
+    <div>
+        <br/>
+        <ul className="list-inline">
+            <li><i className="fa fa-2x fa-envelope-o" aria-hidden="true"></i></li>
+            <li><i className="fa fa-2x fa-github" aria-hidden="true"></i></li>
+            <li><i className="fa fa-2x fa-linkedin" aria-hidden="true"></i></li>
+            <li><i className="fa fa-2x fa-codepen" aria-hidden="true"></i></li>
+        </ul>
+    </div>
+</div>
+</div>
 
 export default Contact
